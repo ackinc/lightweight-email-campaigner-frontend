0f98bc0a2a96c824211d6771dc5b852310e3d3e0
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2019-01-16 20:04:56 +0530

    Added react-google-login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { GoogleLogin } from 'react-google-login';
 import './App.css';
+
+const CLIENT_ID = '748917549487-hoeukkchunsdntek65gepkdgs8nsgpio.apps.googleusercontent.com';
+
+function responseGoogle(response) {
+  console.log(response);
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <GoogleLogin
+          clientId={CLIENT_ID}
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+        />
       </div>
     );
   }
