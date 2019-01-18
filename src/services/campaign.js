23ff250b5ba018a717c60aafccbfa10f337af0d1
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2019-01-18 13:08:52 +0530

    Added campaign service

---
+++ src/services/campaign.js
@@ -0,0 +1,15 @@
+import { get, post } from './http';
+
+export async function getCampaigns() {
+  const { campaigns } = await get('/campaigns', null, true);
+  return campaigns;
+}
+
+export async function getLeadsForCampaign(campaignId) {
+  const { leads } = await get(`/campaigns/${campaignId}/leads`, null, true);
+  return leads;
+}
+
+export async function createCampaign(name, subject, body, leads) {
+  await post('/campaigns', { name, subject, body, leads }, true);
+}
